a99fef8df2a2a32833174738056b78cdefa382fa
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-06-01 09:08:16 +0530

    Changes on abhay branch

--- const.js
+++ const.js
@@ -2,6 +2,6 @@
 
 // this is a comment added
 
-let a=10; // changed const to let on branch abhay // a constant must need to be initialised with a // this line was modified on branch abhay value at time of declaration
+let a=10;//changes on abhay // changed const to let on branch abhay // a constant must need to be initialised with a // this line was modified on branch abhay value at time of declaration
 // we cant reassign a constant
 console.log(a); // using a variable || displaying the value of the variable in the console.
