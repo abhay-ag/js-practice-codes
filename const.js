1629d94d05c5763b5716059c60bcc09d43490fa8
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-06-01 09:03:36 +0530

    Spelling fixes || added description

--- const.js
+++ const.js
@@ -1,5 +1,5 @@
 // declaring a constant
 
 const a=10; // a constant must need to be initialised with a value at time of declaration
-// we cant reasign a constant
-console.log(a); // using a variable
+// we cant reassign a constant
+console.log(a); // using a variable || displaying the value of the variable in the console.
