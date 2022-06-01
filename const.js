7885f56fb1cde97fadf4b50dbefb0923bd784d70
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-06-01 09:06:30 +0530

    Raising merge conflicts || made changes

--- const.js
+++ const.js
@@ -1,5 +1,7 @@
 // declaring a constant
 
-const a=10; // a constant must need to be initialised with a value at time of declaration
+// this is a comment added
+
+let a=10; // changed const to let on branch abhay // a constant must need to be initialised with a // this line was modified on branch abhay value at time of declaration
 // we cant reassign a constant
 console.log(a); // using a variable || displaying the value of the variable in the console.
